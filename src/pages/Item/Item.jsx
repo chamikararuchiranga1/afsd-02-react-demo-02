f5de88e9fa5f3b115f3e9ca11a434046284d15c0
chami <chamikararuchiranga1@gmail.com>
2023-10-21 08:27:51 +0530

    create axios order instance

--- src/pages/Item/Item.jsx
+++ src/pages/Item/Item.jsx
@@ -1,20 +1,31 @@
-import axios from "axios";
 import {useEffect, useState} from "react";
 import UserCard from "../../component/userCard/UserCard.jsx";
 import Box from "@mui/material/Box";
+import instance from '../../services/AxiosOrder.jsx'
+import Button from "@mui/material/Button";
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
+import TextField from "@mui/material/TextField";
 
-export default function Item () {
+export default function Item() {
 
     const [data, setData] = useState([]);
     const [loader, setLoader] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [id, setId] = useState('');
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
 
-    useEffect(()=> {
+    useEffect(() => {
         setLoader(true)
         getAllData();
-    },[])
+    }, []);
 
-    function getAllData () {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    function getAllData() {
+        instance.get('/posts')
             .then(function (response) {
                 setData(response.data);
                 setLoader(false);
@@ -24,19 +35,75 @@ export default function Item () {
             })
     }
 
+    function saveData () {
+        const data = {
+            title: title,
+            body: body,
+            userId: id,
+        }
+        instance.post('/posts', data)
+            .then(function (response) {
+                console.log(response)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
     return (
         <div>
+            <Box sx={{display: 'flex', justifyContent: 'end'}}>
+                <Button variant="contained" onClick={() => setOpen(true)}>Add Data</Button>
+            </Box>
             {loader ?
                 <Box sx={{display: 'flex', justifyContent: 'center'}}>
                     <h1>Loading..</h1>
                 </Box>
                 :
                 <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
-                    {data.map((val, index)=> (
+                    {data.map((val, index) => (
                         <UserCard key={index} userID={val.id} title={val.title} description={val.body}/>
                     ))}
                 </Box>
             }
+            <Box>
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>Add Data</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            value={id}
+                            onChange={(val)=> {setId(val.target.value)}}
+                            margin={'dense'}
+                            fullWidth
+                            id="outlined-basic"
+                            label="User Id"
+                            variant="outlined"
+                        />
+                        <TextField
+                            value={title}
+                            onChange={(val)=> {setTitle(val.target.value)}}
+                            margin={'dense'}
+                            fullWidth
+                            id="outlined-basic"
+                            label="Title"
+                            variant="outlined"
+                        />
+                        <TextField
+                            value={body}
+                            onChange={(val) => {setBody(val.target.value)}}
+                            margin={'dense'}
+                            fullWidth
+                            id="outlined-basic"
+                            label="Body"
+                            variant="outlined"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" onClick={() => handleClose()}>Cancel</Button>
+                        <Button variant="contained" onClick={()=> saveData()}>Save</Button>
+                    </DialogActions>
+                </Dialog>
+            </Box>
         </div>
     )
 }
